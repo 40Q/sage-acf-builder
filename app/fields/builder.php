1f3c0261a0f3d8c375e4f5e1082e9ce19c81331c
José Debuchy <jdebuchy@40q.com.ar>
2018-09-07 19:52:12 -0300

    Include global settings

--- app/fields/builder.php
+++ app/fields/builder.php
@@ -361,4 +361,21 @@ $fields['page_content']
         'the_content',
     ]);
 
+/**
+ * Global Settings
+ */
+$fields['global_settings'] = new FieldsBuilder('global_settings');
+$fields['global_settings']
+    ->addTab('header', ["placement" => "left"])
+        ->addImage('logo', [
+            'preview_size' => 'medium'
+        ])
+    ->addTab('footer', ["placement" => "left"])
+        ->addImage('footer_logo', [
+            'preview_size' => 'medium'
+        ])
+    ->setLocation('options_page', '==', 'global-options');
+
+return $fields;
+
 return $fields;
